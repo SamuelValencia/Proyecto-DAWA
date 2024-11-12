d97db9d534ffc9ec27534b5503fbee1222b379a3
Samuel <samuelvalencia780@gmail.com>
2024-11-12 00:21:06 -0500

    nuevo

--- src/app/components/aprobados/aprobados.component.spec.ts
+++ src/app/components/aprobados/aprobados.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AprobadosComponent } from './aprobados.component';
 
 describe('AprobadosComponent', () => {
---
+++ src/app/components/aprobados/aprobados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aprobados',
+  standalone: true,
+  imports: [],
+  templateUrl: './aprobados.component.html',
+  styleUrl: './aprobados.component.css'
+})
+export class AprobadosComponent {
+
+}
